24a305e12ff2fe8b2b043e65bb4c27337b7dfa1a
maksim.gorban <maksim.gorban@glowbyteconsulting.com>
2024-11-27 13:41:30 +0300

    Added test pipeline

--- app/main.py
+++ app/main.py
@@ -1,7 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.config import DB_URL, DEV_MODE
 from app.routes import home, authentication
 from app.setup_db import async_engine, Base
 
@@ -12,7 +11,6 @@ app = FastAPI(title="Portfolio API")
 
 @app.on_event("startup")
 async def init_database():
-    print(DEV_MODE, DB_URL)
     async with async_engine.begin() as connection:
         await connection.run_sync(Base.metadata.create_all)
 
